d5dd9a615f94b2e868ae36a6aa2d659420b304ea
Steinar Bragi Sigurðarson <steinar.bragi@gmail.com>
2019-08-25 21:09:29 +0000

    update description

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,9 +20,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        name: `Steinar Bragi Sigurðarson`,
+        short_name: `steinar.io`,
+        description: `Web and Mobile developer`,
         start_url: `/`,
         background_color: `#141821`,
         theme_color: `#f6ad55`,
